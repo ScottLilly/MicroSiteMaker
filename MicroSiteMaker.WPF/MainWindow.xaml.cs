145a9e01d4883ef58048957c6bf1dba1029cc113
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-03-18 18:30:21 -0500

    Add project files.

---
+++ MicroSiteMaker.WPF/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace MicroSiteMaker.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Exit_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
