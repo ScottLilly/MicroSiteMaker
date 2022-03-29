567437e7563fff739df8eba9fb9c4541af1e2c6c
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-03-28 22:06:05 -0500

    Create default directories

--- MicroSiteMaker.Services/SiteBuilderService.cs
+++ MicroSiteMaker.Services/SiteBuilderService.cs
@@ -1,10 +1,25 @@
 ﻿using System.Text;
+using System.Text.RegularExpressions;
 using MicroSiteMaker.Models;
+using Microsoft.VisualBasic;
 
 namespace MicroSiteMaker.Services;
 
 public static class SiteBuilderService
 {
+    public static void BuildDirectories(string path)
+    {
+        Directory.CreateDirectory(path);
+
+        Directory.CreateDirectory(Path.Combine(path, "Input"));
+        Directory.CreateDirectory(Path.Combine(path, "Input/Pages"));
+        Directory.CreateDirectory(Path.Combine(path, "Input/Templates"));
+        Directory.CreateDirectory(Path.Combine(path, "Input/Images"));
+        Directory.CreateDirectory(Path.Combine(path, "Input/Snippets"));
+
+        Directory.CreateDirectory(Path.Combine(path, "Output"));
+    }
+
     public static Dictionary<string, string> GetWebPageFiles(WebSite webSite)
     {
         Dictionary<string, string> files =
@@ -44,10 +59,25 @@ public static class SiteBuilderService
     {
         StringBuilder text = new StringBuilder();
 
-        text.AppendLine("div {");
-        text.AppendLine("   width: 500px;");
-        text.AppendLine("   margin: auto;");
+        text.AppendLine(".left, .right {");
+        text.AppendLine("   float: left;");
+        text.AppendLine("   width: 20%;");
         text.AppendLine("}");
+        text.AppendLine(".main {");
+        text.AppendLine("   float: left;");
+        text.AppendLine("   width: 60%;");
+        text.AppendLine("}");
+        // If the screen is less than 800 pixels wide, use 100% of it
+        text.AppendLine("@media screen and (max-width: 800px) {");
+        text.AppendLine("   .left, .main, .right {");
+        text.AppendLine("      width: 100%;");
+        text.AppendLine("   }");
+        text.AppendLine("}");
+
+        //text.AppendLine("div {");
+        //text.AppendLine("   width: 500px;");
+        //text.AppendLine("   margin: auto;");
+        //text.AppendLine("}");
         text.AppendLine("h1 {");
         text.AppendLine("   text-align: center;");
         text.AppendLine("   color: #0000b8;");
