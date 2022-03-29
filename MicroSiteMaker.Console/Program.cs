83628c4e64ab0335dbb4ddc0f1ce6f8ad0ad0b6d
Scott Lilly <2271154+ScottLilly@users.noreply.github.com>
2022-03-29 09:32:18 -0500

    Added --help message

--- MicroSiteMaker.Console/Program.cs
+++ MicroSiteMaker.Console/Program.cs
@@ -7,9 +7,17 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        // Parse arguments and show error messages, if any
         var argDictionary = ArgParser.GetArgumentDictionary(args);
 
+        // If user passes in --help parameter, display help message and stop running
+        if (argDictionary.ContainsKey("--help"))
+        {
+            ArgParser.DisplayHelp();
+            return;
+        }
+
+        // Build the Website object and check for errors with the parameters.
+        // Stop running if there are invalid, or missing required, parameters.
         var website = new WebSite(argDictionary);
 
         if (website.HasErrors)
